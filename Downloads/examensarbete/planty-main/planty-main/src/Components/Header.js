baa7fcb4ca872cc1dc72b57893067b52a48a4cd9
Veronica Torell <veronicasofie@hotmail.com>
2023-01-22 15:30:39 +0100

    Style: Adds hamburger/mobile menu and shows cart on icon hover

--- Downloads/examensarbete/planty-main/planty-main/src/Components/Header.js
+++ Downloads/examensarbete/planty-main/planty-main/src/Components/Header.js
@@ -5,6 +5,7 @@ import Checkout from '../Pages/Checkout';
 import {DropdownCart, Dropdown} from "../Styles/headerStyle"
 import {FaShoppingCart} from "react-icons/fa"
 import logo from "C:/Users/veron/Downloads/examensarbete/planty-main/planty-main/src/assets/logo21.png"
+import cartlogo from "C:/Users/veron/Downloads/examensarbete/planty-main/planty-main/src/assets/cart-logo.jpg"
 
 
 
@@ -29,15 +30,73 @@ function Header({cartProducts, setCartProducts}) {
   <div className='logo'>
 <img height={350} width ={700} src={logo} alt="Logo" />
 </div>
+
+
+<div className="dropdown">
+
+    <button className="button2"> 
+    <img height={50} width ={60} src={cartlogo} alt="Logo" />
+
+ </button>
+
+    <div className="dropdown-content"  >
+
+<Cart cartProducts={cartProducts} setCartProducts={setCartProducts} toggleCart={toggleCart} setToggleCart={setToggleCart}/>
+
+    </div>
+
+  </div>
+
  
 
-<div className='navigation'>
+ 
+
+<nav role="navigation">
+
+  <div id="menuToggle">
+
+   
+
+    <input type="checkbox" />
+
+   
+
+    <span></span>
+
+    <span></span>
+
+    <span></span>
+
+   
+
+    <ul id="menu">
+
+      <a href="/"><li>Home</li></a>
+
+      <a href="/Products"><li>Products</li></a>
+
+      <a href="/About"><li>About</li></a>
+
+      <a href="Tips"><li>Info</li></a>
+
+      <a href="Contact"><li>Contact</li></a>
+
+    </ul>
+
+  </div>
+
+</nav>
+
+
+ 
+
+{/* <div className='navigation'>
  
  <Link to ="/">Home</Link>
  <Link to ="/About">About us</Link>
  <Link to ="/Products">Our products</Link> 
  <Link to ="/Tips">Tips</Link>
- </div>
+ </div> */}
 
 {/* <Cart cartProducts={cartProducts} setCartProducts={setCartProducts} toggleCart={toggleCart} setToggleCart={setToggleCart}/> */}
 
